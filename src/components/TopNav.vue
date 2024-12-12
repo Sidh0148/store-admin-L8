b7a19a8a329ec30edd8c4605e1d77675124df359
Aakash <sidh0148@algonquinlive.com>
2024-12-12 18:21:46 -0500

    new

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -40,7 +40,7 @@ nav {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #0046be; /* BestBuy blue */
+  background-color: #0248c1; /* BestBuy blue */
   color: white;
   padding-top: 0.5rem;
   padding-left: 1rem;
