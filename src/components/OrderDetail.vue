ca27690ddea479be0331b0b28ff6d35071acfc90
Aakash <sidh0148@algonquinlive.com>
2024-12-12 00:50:46 -0500

    new

--- src/components/OrderDetail.vue
+++ src/components/OrderDetail.vue
@@ -3,7 +3,7 @@
     <div class="action-button">
       <button @click="completeOrder" class="button">Complete Order</button>
     </div>
-    <br/>
+    <br />
     <div class="order-header">
       <p><b>Order ID:</b> {{ order.orderId }}</p>
       <p><b>Customer ID:</b> {{ order.customerId }}</p>
@@ -29,10 +29,7 @@
         </tr>
         <tfoot>
           <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td colspan="4" style="text-align: right;"><b>Total:</b></td>
             <td><b>{{ orderTotal() }}</b></td>
           </tr>
         </tfoot>
@@ -40,83 +37,114 @@
     </div>
   </div>
   <div class="order-detail" v-else>
-    <h3>Opps! That order was not found...</h3>
+    <h3>Oops! That order was not found...</h3>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'OrderDetail',
-    props: ['orders','products'],
-    emits: ['completeOrder'],
-    data() {
-      return {
-        order: null
-      }
+export default {
+  name: 'OrderDetail',
+  props: ['orders', 'products'],
+  emits: ['completeOrder'],
+  data() {
+    return {
+      order: null,
+    };
+  },
+  computed: {
+    orderExists() {
+      return !!this.order;
     },
-    computed: {
-      orderExists() {
-        return !!this.order
+  },
+  mounted() {
+    this.getOrder();
+  },
+  methods: {
+    getOrder() {
+      this.order = this.orders.find(order => order.orderId === this.$route.params.id);
+
+      if (!this.order) {
+        fetch(`/makeline/order/${this.$route.params.id}`)
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+            if (response.status === 204) {
+              return null;
+            }
+            return response.json();
+          })
+          .then(data => {
+            if (data) {
+              this.order = data;
+            } else {
+              console.log('No orders from server');
+            }
+          })
+          .catch(error => console.error(error));
       }
+
+      return this.order;
     },
-    mounted() {
-      this.getOrder()
+    completeOrder() {
+      this.$emit('completeOrder', this.order.orderId);
     },
-    methods: {
-      getOrder() {
-        // get the order from the orders prop
-        // if not found in the orders prop, fetch from the server
-        this.order = this.orders.find(order => order.orderId === this.$route.params.id);  
-        
-        if (!this.order) {          
-          // get the order from the makeline service
-          fetch(`/makeline/order/${this.$route.params.id}`)
-            .then(response => {
-              if (!response.ok) {
-                throw new Error('Network response was not ok');
-              }
-              // check if the response is empty
-              if (response.status === 204) {
-                return null;
-              }
-              return response.json();
-            })
-            .then(data => {
-              if (data) {
-                this.order = data;
-              } else {
-                console.log('No orders from server');
-              }
-            })
-            .catch(error => console.error(error));
-        }
-        
-        return this.order;
-      },
-      completeOrder() {
-        this.$emit('completeOrder', this.order.orderId)
-      },
-      productLookup(id) {
-        return this.products.find(product => product.id === id).name;
-      },
-      orderTotal() {
-        let total = 0;
-        this.order.items.forEach(item => {
-          total += item.price * item.quantity;
-        });
-        return total.toFixed(2);
-      }
-    }
-  }
+    productLookup(id) {
+      return this.products.find(product => product.id === id).name;
+    },
+    orderTotal() {
+      let total = 0;
+      this.order.items.forEach(item => {
+        total += item.price * item.quantity;
+      });
+      return total.toFixed(2);
+    },
+  },
+};
 </script>
 
 <style scoped>
 a {
-  color: #0000FF;
+  color: #0046be; /* BestBuy blue for links */
   text-decoration: underline;
 }
 
 .order-detail {
   text-align: left;
+}
+
+table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 1rem;
+  border: 1px solid #ddd;
+}
+
+th,
+td {
+  padding: 8px;
+  text-align: left;
+}
+
+th {
+  background-color: #0046be; /* BestBuy blue */
+  color: white;
+}
+
+tfoot td {
+  font-weight: bold;
+}
+
+button {
+  padding: 10px 20px;
+  background-color: #0046be; /* BestBuy blue */
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #003bb3; /* Darker shade for hover */
 }
 </style>
